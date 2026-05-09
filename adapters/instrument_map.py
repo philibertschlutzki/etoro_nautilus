3e57b451077da7a0de8e79181ef66c0f89a40cdb
macbook <yourmail@mommy.com>
2026-05-09 10:18:18 +0200

    update

--- adapters/instrument_map.py
+++ adapters/instrument_map.py
@@ -12,4 +12,10 @@ ETORO_INSTRUMENTS = {
     "1137": "NVDA.ETORO",
     "5045": "FSLY.ETORO",
     "9119": "INSM.ETORO",
+    "9119": "INSM.ETORO",
+    "100000": "BTC.ETORO",
+    "100001": "ETH.ETORO",
+    "100017": "ADA.ETORO",
+    "100003": "XRP.ETORO",
+    "100421": "AERO.ETORO",
 }
