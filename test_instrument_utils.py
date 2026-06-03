2975b94248a72dedd57b0e5a5296dbad783d7623
philibertschlutzki <77938082+philibertschlutzki@users.noreply.github.com>
2026-06-03 20:24:02 +0200

    Delete test_instrument_utils.py

--- test_instrument_utils.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from automation.adapters.instrument_utils import get_size_precision
-
-def test_get_size_precision():
-    assert get_size_precision("BTC.ETORO") == 8
-    assert get_size_precision("AAPL.ETORO") == 2
