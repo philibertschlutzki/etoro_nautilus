6a4101e98c8579e60b8114e2e81b956e2b1e9bd4
philibertschlutzki <77938082+philibertschlutzki@users.noreply.github.com>
2026-06-29 07:11:02 +0200

    Delete fix_tests.py

--- fix_tests.py
+++
@@ -1,7 +0,0 @@
-with open("automation/tests/test_issue_468_penalty_conditioning.py", "r") as f:
-    text = f.read()
-
-text = text.replace('"oos_min_trades": 10,', '"oos_min_trades": 10, "oos_min_sortino": 0.3, "oos_min_profit_factor": 1.1,')
-
-with open("automation/tests/test_issue_468_penalty_conditioning.py", "w") as f:
-    f.write(text)
